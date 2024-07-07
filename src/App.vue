fd8d551788e04587b97a52ee59be2170d0a353a5
ccyuen1 <143187971+ccyuen1@users.noreply.github.com>
2024-07-07 18:50:45 +0800

    move code into repo

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="text-center font-bold text-2xl">ccyuen1</header>
+  <hr />
+  <div class="flex items-stretch">
+    <div>
+      <nav class="overflow-auto">NAVIGATION</nav>
+      <hr />
+    </div>
+    <div class="overflow-auto flex-grow">
+      <main>MAIN</main>
+      <hr />
+      <footer class="text-center">
+        This page is created by ccyuen1 using TypeScript, Vue, Vite, and
+        Tailwind CSS.
+        <br />
+        Copyright &copy; 2024 ccyuen1, All Rights Reserved.
+      </footer>
+    </div>
+  </div>
+</template>
